3266187992eafd533bab940488ecd2341cf6bb6e
massygh <massyghen1@gmail.com>
2023-10-16 15:02:57 -0700

    number

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number <= 9; number++)
+	for (number = 0; number < 10; number++)
 	{
 		putchar(number + '0');
-		if (number <= 9)
+		if (number < 9)
 		{
 			putchar(',');
 			putchar(' ');
