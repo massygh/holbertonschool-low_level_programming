53de77b9453adc41ec7df1406e3f47c154b42801
massygh <massyghen1@gmail.com>
2023-10-16 14:30:10 -0700

    number

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - This program will prints the alphabet in and then in uppercase
+ *
+ * Return: 0 is succes
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+		{
+			putchar(number);
+
+		if (number < 10)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0);
+}
