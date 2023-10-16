8ab84ba562220f309130e516ab35768d753655d1
massygh <massyghen1@gmail.com>
2023-10-16 15:01:12 -0700

    number

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int number;
 
-	for (number = '0'; number <= '9'; number++)
+	for (number = 0; number <= 9; number++)
 	{
-		if (number > 10)
-			{
+		putchar(number + '0');
+		if (number <= 9)
+		{
 			putchar(',');
 			putchar(' ');
-			}
-			putchar(number);
+		}
 	}
 	putchar('\n');
 
