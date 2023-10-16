c39c25e1ac739f1fbf856c25adc357f811a90ca7
massygh <massyghen1@gmail.com>
2023-10-16 13:00:19 -0700

    number

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This program will prints the alphabet in lowercase
+ *
+ * Return: 0 is succes
+ */
+
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
