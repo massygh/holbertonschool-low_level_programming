7757222c9155ba1b82e605b4d29e0280f6890318
massygh <massyghen1@gmail.com>
2023-10-16 07:23:35 -0700

    Alphabet

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This program will prints the alphabet in and then in uppercase
+ *
+ * Return: 0 is succes
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++){
+		if (letter != 'q' && letter != 'e' ) {
+                putchar(letter);
+		}
+	}
+        putchar('\n');
+	return (0);
+}
