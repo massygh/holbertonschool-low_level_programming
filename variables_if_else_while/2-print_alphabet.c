c4e4f912df6c04630ddf17d750fa0764402b368c
massygh <massyghen1@gmail.com>
2023-10-16 06:11:22 -0700

    Alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This program will prints the alphabet in lowercase
+ *
+ * Return: 0 is succes
+ */
+
+int main(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+}
