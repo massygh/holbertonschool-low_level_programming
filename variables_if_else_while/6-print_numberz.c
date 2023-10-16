250979c280ec5160cc806585f8413ba872f47cba
massygh <massyghen1@gmail.com>
2023-10-16 13:07:30 -0700

    number

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This program will prints the alphabet in and then in uppercase
+ *
+ * Return: 0 is succes
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = '0'; number <= '0'; number++)
+		putchar(number);
+	putchar('\n');
+	return (0);
+}
