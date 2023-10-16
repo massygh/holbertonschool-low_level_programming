3e13504faaadad2d746e820bd50fb14b46722f4f
massygh <massyghen1@gmail.com>
2023-10-16 06:44:48 -0700

    Alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This program will prints the alphabet in lowercase, and then in uppercase
+ *
+ * Return: 0 is succes
+ */
+
+int main(void)
+{
+	char letter;
+	char LETTER;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (LETTER = 'A'; LETTER <= 'Z'; LETTER++)
+                putchar(LETTER);
+	putchar('\n');
+	return (0);
+}
