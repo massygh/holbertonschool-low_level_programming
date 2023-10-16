4e6742cea059ecc21a7908b60eabcfd08f4f0ada
massygh <massyghen1@gmail.com>
2023-10-16 05:48:57 -0700

    last degit code

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This program will assign a random num the variable
+ *
+ * Return: 0 is succes
+ */
+
+int main(void)
+{
+	int n;
+	int z;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	z = n % 10;
+	if (z == 0)
+	{
+		 printf("Last digit of %d is %d and is 0\n", n, z);
+	}
+	else if (z > 5)
+	{
+		printf("Last digit of %d  is %d and is greater than 5\n", n, z);
+	}
+	else
+	{
+		 printf("Last digit of %d  is %d and is less than 6 and not 0\n", n, z);
+	}
+	return (0);
+}
