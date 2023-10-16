6f0deed81e2f755ef37eaf5e84efb6da9ac0cb44
massygh <massyghen1@gmail.com>
2023-10-16 07:28:52 -0700

    Alphabet

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,10 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++) {
-		if (letter != 'q' && letter != 'e' ) {
+	for (letter = 'a'; letter <= 'z'; letter++) 
+	{
+		if (letter != 'q' && letter != 'e' ) 
+		{
 		putchar(letter);
 		}
 	}
