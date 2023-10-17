95794b44ee8d69f6256a704f426eafc1b06d05ad
massygh <massyghen1@gmail.com>
2023-10-17 06:21:49 -0700

    Functions nested loops

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - use prototype
+ * _islower - Write a function that checks for lowercase character.
+ * @c - int
+ */
+
+int _islower(int c)
+
+{
+	{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+	}
+}
