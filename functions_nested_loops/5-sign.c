3c19925491c0d281d4c116331fe4b827966c8b2f
massygh <massyghen1@gmail.com>
2023-10-18 13:44:06 -0700

    print sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,33 +4,18 @@
 /**
  * print_alphabet_x10 - use prototype
  *
- */
+*/
 
-int print_sign(int n) 
-{
-	if (n > 0) 
-	{
-		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(1 + '0');
-	} 
-	else if (n == 0)
-	{
-		_putchar(0 + '0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(0 + '0');
-	} 
-	else 
-	{
-		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
-		_putchar('-');
-		_putchar(1 + '0');
-	}
-
-	putchar('\n');
-	return n;
+int print_sign(int n) {
+    
+	if (n > 0) {
+        _putchar('+');
+        return 1;
+    } else if (n < 0) {
+        _putchar('-');
+        return -1;
+    } else {
+        _putchar('0');
+        return 0;
+    }
 }
