3785d42943cd3dba699495fbd5014fe2a9826373
massygh <massyghen1@gmail.com>
2023-10-19 05:51:25 -0700

    alphabet

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - use prototype
+ *
+ */
+
+int _isupper(int c)
+{
+	{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+	}
+} 
