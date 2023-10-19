ef8323b81649bce57529f5ce66a760760b37423f
massygh <massyghen1@gmail.com>
2023-10-19 02:48:48 -0700

    absolute value

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int)
+
+	int a;
+	int b;
+
+	a = n;
+	b = a % 10;
+	result = "%d %d, a,z);
+	putchar(result)
+	return (b);
