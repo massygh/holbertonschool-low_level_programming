38dc1bfbbe12576a49cc7785510d19eed8627cd3
massygh <massyghen1@gmail.com>
2023-10-19 06:47:25 -0700

    digit

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - Write a function that checks for lowercase character.
+ * @c: int
+ * Return: 0 is succes
+ */
+
+void print_numbers(void);
+
+{
+	int a;
+
+	for (a = '0' ; a <= '9'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
