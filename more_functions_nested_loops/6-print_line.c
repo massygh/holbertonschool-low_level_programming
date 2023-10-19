6c2a8cc065bf6e33c3e742d6ecac2678bc62a42b
massygh <massyghen1@gmail.com>
2023-10-19 09:05:46 -0700

    print 10x times

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - print number
+ * Return: 0 is succes
+ */
+
+void print_line(int n)
+
+{
+	if (n <= 0) 
+	{
+		_putchar('\n');
+	} 
+	else if (n > 0)
+	{
+		_putchar('_');
+		_putchar('\n');
+	}
+}
