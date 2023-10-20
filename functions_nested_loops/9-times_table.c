908b8ffd9d01357881d77430ba28e2b194680f02
massygh <massyghen1@gmail.com>
2023-10-20 07:24:43 -0700

    prints the 9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ * Return: 0 is succes
+ */
+
+void times_table(void)
+
+{
+	int i, j;
+	int product;
+
+	for (i = 0; i <= 9; i++) {
+		for (j = 0; j <= 9; j++)
+		{
+
+			if (j > 0) 
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			if (product = i * j, product < 10)
+			{
+				putchar(' ');
+				putchar(product + '0');
+			} 
+			else 
+			{
+				putchar(product / 10 + '0');
+				putchar(product % 10 + '0');
+			}
+		}
+		putchar('\n');
+	}
+}
+
