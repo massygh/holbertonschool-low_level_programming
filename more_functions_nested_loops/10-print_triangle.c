e9bb0d5538ab40cb1a0ec3751a7749994a3f56ac
massygh <massyghen1@gmail.com>
2023-10-20 00:52:07 -0700

    print square

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - print ten times
+ * @size - main
+ * Return: 0 is succes
+ */
+
+void print_triangle(int size)
+
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
