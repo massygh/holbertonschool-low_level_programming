c40b3c770a72f15d0fa485aaec039a1245a23503
massygh <massyghen1@gmail.com>
2023-10-20 07:32:09 -0700

    24 hours

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,22 +12,22 @@ void times_table(void)
 	int i, j;
 	int product;
 
-	for (i = 0; i <= 9; i++) 
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 
-			if (j > 0) 
+			if (j > 0)
 			{
 				putchar(',');
 				putchar(' ');
 			}
-			if (product = i * j, product < 10)
+			else if (product = i * j, product < 10)
 			{
 				putchar(' ');
 				putchar(product + '0');
-			} 
-			else 
+			}
+			else
 			{
 				putchar(product / 10 + '0');
 				putchar(product % 10 + '0');
