1c57b74173e755ce4faccea2cd173852ab52c3e0
massygh <massyghen1@gmail.com>
2023-10-20 04:16:25 -0700

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,19 +8,34 @@
 
 int main(void)
 
-{	
+{
 	int n;
 
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3) == 0 && (n % 5) == 0)
+		{
 			printf("FizzBuzz");
+			printf(" ");
+		}
+		
 		else if ((n % 5) == 0 )
+		{
 			printf("Buzz");
+			printf(" ");
+		}
+		
 		else if ((n % 3) == 0 )
+		{
 			printf("Fizz");
+			printf(" ");
+		}
+		
 		else
+		{
 			printf("%d" ,n);
+			printf(" ");
+		}
 	}
 	return(0);
 }			
