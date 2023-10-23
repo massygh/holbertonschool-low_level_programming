c78c31e41779f74e3492e52088cabd58dd7ce7ad
massygh <massyghen1@gmail.com>
2023-10-23 01:18:47 -0700

    value it points to

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 = updates the value it points to to 98
+ *@n: variable
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+
+{
+    if (n != NULL) 
+	{
+	*n = 98;
+	}
+}
+
