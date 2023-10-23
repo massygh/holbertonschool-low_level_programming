4f689809777f9b4f814e94a4a8c2b8735af0035c
massygh <massyghen1@gmail.com>
2023-10-23 03:03:04 -0700

    value it points to

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int _strlen - returns the length of a string.
+ * @s: variable
+ */
+
+int _strlen(char *s)
+
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++; 
+		s++;
+	}
+
+	return length;
+}
+
