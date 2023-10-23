8934d33037be28e1f7862080e8426419699ab6f4
massygh <massyghen1@gmail.com>
2023-10-23 07:10:58 -0700

    character of a string

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -12,11 +12,13 @@ void puts2(char *str)
 
 {
 	int i = 0;
-
 	while (str[i] != '\0')
 	{
-		putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+		i++;
 	}
 	putchar('\n');
 }
