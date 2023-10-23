c86681e6c0cb5a61081edfa4273f6801c0fd5a59
massygh <massyghen1@gmail.com>
2023-10-23 13:26:46 -0700

    prints half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string
+ * @s: variable
+ */
+
+void puts_half(char *str)
+
+{
+	int length = strlen(str);
+	int start_index;
+	int i;
+
+	if (length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+	else
+	{
+		start_index = (length - 1) / 2;
+	}
+
+	for (i = start_index; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
