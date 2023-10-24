ad551fdcd09722d885c8f25fdb7d0453e36c9a8c
massygh <massyghen1@gmail.com>
2023-10-24 07:24:31 -0700

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcpy - copy string
+ * @src: variable
+ * @dest: varible2
+ * @n: variable3
+ * Return: Always (dest)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	char *temp = dest;
+
+	while (*temp != '\0')
+	{
+		temp++;
+	}
+
+	while (*src != '\0' && n > 0)
+	{
+		*temp = *src;
+		temp++;
+		src++;
+		n--;
+	}
+
+	*temp = '\0';
+
+	return dest;
+}
