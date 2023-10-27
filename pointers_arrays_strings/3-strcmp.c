d2bdbbfdd9eb646ca96c293ed0aa982f9b0de2c3
massygh <massyghen1@gmail.com>
2023-10-27 02:53:51 -0700

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -15,9 +15,9 @@ int _strcmp(char *s1, char *s2)
 	while (*s1 == *s2)
 	{
 		if (*s1 == '\0')
-			return 0;
+			return (0);
 		s1++;
 		s2++;
 	}
-	return (*(unsigned char *)s1) - (*(unsigned char *)s2);
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
