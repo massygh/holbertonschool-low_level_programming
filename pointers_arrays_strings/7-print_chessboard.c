59adc9e41576eb200a5fb2ba4359a770a197e9e8
massygh <massyghen1@gmail.com>
2023-10-29 06:30:03 -0700

    chess

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: variable
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+	int i;
+	int j;
+
+	{
+		for (i = 0; i < 8; i++)
+		{
+			for (j = 0; j < 8; j++)
+			{
+				putchar(a[i][j]);
+			}
+			putchar('\n');
+		}
+	}
+}
