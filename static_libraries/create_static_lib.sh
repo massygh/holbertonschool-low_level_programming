33d80dd50b50d6e62f0a6eb29c9af36353ee183d
massygh <massyghen1@gmail.com>
2023-11-01 08:04:35 -0700

    pow

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-for fichier_source in *.c
-do
-    if [ -f "$fichier_source" ]; then
-        fichier_objet="${fichier_source%.c}.o"
-        gcc -c "$fichier_source" -o "$fichier_objet"
-    fi
-done
-
+ar rcs liball.a *.o
