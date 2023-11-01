2edd72c95722a5207a815f9212d0a0f60004fcfd
massygh <massyghen1@gmail.com>
2023-11-01 07:04:13 -0700

    Dreams

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * is_prime_number - prime 
+ * is_prime_number - prime
+ * @n: variable 
  * Return: Always 0.
  */
 
@@ -10,7 +11,12 @@ int is_prime_number(int n)
 
 {
 	if ( n %2 != 0 && n %3 != 0 && n %5 != 0 && n > 1)
+	{
 		return(1);
+	}
+	
 	else
+	{
 		return(0);
+	}
 }
