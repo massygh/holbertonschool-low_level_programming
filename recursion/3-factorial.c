7615ab3d2638bdf4d7e8a392bc92e8631abd7688
massygh <massyghen1@gmail.com>
2023-11-01 06:32:31 -0700

    Dreams

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -4,26 +4,23 @@
 /**
  * factorial -  factorial of a given number
  * @n: variable
- * Return: Always 0.
+ * Return: factorial
  */
 
 int factorial(int n)
 
 {
 	if (n < 0)
-
 	{
 		return(-1);
 	}
 
 	else if (n == 1)
-
 	{
 		return(1);
 	}
 
 	else
-
 	{
 		return(n * factorial(n - 1));
 	}
