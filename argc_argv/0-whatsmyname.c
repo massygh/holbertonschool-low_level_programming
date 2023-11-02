86340751e08df018b920758a33c2b2304b7fdace
massygh <massyghen1@gmail.com>
2023-11-02 14:34:05 -0700

    argc

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - program that prints its name
+ **/
+
 int main(int argc, char *argv[])
+
 {
-    if (argc > 0)
-    {
-        printf("%s\n", argv[0]);
-    }
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
 
-    return (0);
+	return (0);
 }
-
