9bf07cadf018b806a0612e73fe77e13a6edd2eab
massygh <massyghen1@gmail.com>
2023-11-03 08:15:34 -0700

    number

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,11 +7,9 @@
  * Return: Always 0.
  **/
 
-int main( __attribute__((unused)) int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 
 {
-                       printf("%s\n", argv[0]);
-   
-
-        return (0);
+    printf("%d\n", argc - 1);
+    return (0);
 }
