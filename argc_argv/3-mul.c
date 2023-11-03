2bb7c5afac7902f494240b3d2a869ad6af7d1d83
massygh <massyghen1@gmail.com>
2023-11-03 13:52:27 -0700

    multiplication

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints its name
+ * @argc: variable
+ * @argv: variable2
+ * Return: Always 0.
+ **/
+
+int main(int argc, char *argv[])
+{
+	int v1 = 0,v2 = 0;
+
+	if (argc == 3)
+	{
+		v1 = atoi(argv[1]);
+		v2 = atoi(argv[2]);
+		printf("%d\n",v1 * v2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
