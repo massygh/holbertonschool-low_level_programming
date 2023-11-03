86e2f34d1566104062c1d980ce6f9dab33bf1a11
massygh <massyghen1@gmail.com>
2023-11-03 14:10:08 -0700

    multiplication

---
+++ argc_argv/4-add.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints its name
+ * @argc: variable
+ * @argv: variable2
+ * Return: Always 0.
+ **/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int c;
+	int sum;
+	char *s;
+
+	sum = 0;
+	c = 0;
+
+	i = 1;
+
+	while (i < argc)
+	{
+		s = argv[i];
+		j = 0;
+		while (s[j] != '\0')
+		{
+			if (s[j] < '0' || s[j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		c = atoi(argv[i]);
+		sum = sum + c;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
