ec33432f99bbfdf2c67cdd8631cec172f7f61918
massygh <massyghen1@gmail.com>
2023-11-03 08:35:40 -0700

    print

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name
+ * @argc: variable
+ * @argv: variable2
+ * Return: Always 0.
+ **/
+
+int main(int argc, char *argv[])
+
+{
+	int i;
+
+	for ( i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
