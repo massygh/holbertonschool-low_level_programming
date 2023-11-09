f97a4d31864a6655937c28322bf01282f985e57c
massygh <massyghen1@gmail.com>
2023-11-09 13:18:02 -0800

    test

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,28 +6,50 @@
  * str_concat - Entry point
  * @s1: String 1
  * @s2: String 2
- * Returns: pointer should point to a newly allocated space in memory or NULL
+ * Return: pointer
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *result;
-	int len1 = 0, len2 = 0;
+	int len1 = 0,
+	int len2 = 0;
 	int i;
+	char *result;
 
-	if (s1 == NULL) s1 = "";
-	if (s2 == NULL) s2 = "";
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
 
-	while (s1[len1]) len1++;
-	while (s2[len2]) len2++;
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
 
 	result = (char *)malloc(len1 + len2 + 1);
-
-	if (result)
+	if (result == NULL)
 	{
-		for (i = 0; (result[i] = s1[i]); i++);
-		for (i = 0; (result[len1 + i] = s2[i]); i++);
+		return (NULL);
 	}
+	for (i = 0; i < len1; i++)
+	{
+		result[i] = s1[i];
+	}
+
+	for (i = 0; i < len2; i++)
+	{
+		result[len1 + i] = s2[i];
+	}
+
+	result[len1 + len2] = '\0';
 
 	return (result);
 }
