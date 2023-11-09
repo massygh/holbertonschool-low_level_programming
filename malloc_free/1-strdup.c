e336f317bf76bc9c11dfd02b38adcb718dd80c7b
massygh <massyghen1@gmail.com>
2023-11-09 01:01:20 -0800

    test1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 		return (NULL);
 	for (n = 0; str[n] != '\0'; n++)
 
-	strnew = (char *)malloc(n + 1 * sizeof(char));
+		strnew = (char *)malloc(n + 1 * sizeof(char));
 	if (strnew != NULL)
 	{
 		for (i = 0; str[i] != '\0'; i++)
