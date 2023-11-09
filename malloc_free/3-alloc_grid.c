7285f7fea17f892edf0c2b0f4a76350ece55713c
massygh <massyghen1@gmail.com>
2023-11-09 04:46:31 -0800

    test

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	int i;
 	int **grid = (int **)malloc(height * sizeof(int *));
 
-	if (width <= 0 || height <= 0)
+	if (width <= 0 && height <= 0)
 	{
 		return (NULL);
 	}
