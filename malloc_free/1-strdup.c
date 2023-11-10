0d95a128d595ef52caa2289d907db76b3aa535f2
massygh <massyghen1@gmail.com>
2023-11-10 00:50:52 -0800

    test

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,27 +9,31 @@
  */
 
 char *_strdup(char *str)
-
 {
-	char *strnew = NULL;
-	unsigned int i;
-	int n;
+	int length = 0;
+	int i;
+	char *duplicate;
 
 	if (str == NULL)
-		return (NULL);
-
-	for (n = 0; str[n] != '\0'; n++)
-
-		strnew = (char *)malloc((n + 1) * sizeof(char));
-	if (strnew != NULL)
-	{
-		for (i = 0; str[i] != '\0'; i++)
-			strnew[i] = str[i];
-	}
-	else
 	{
 		return (NULL);
 	}
-	strnew[i] = '\0';
-	return (strnew);
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	duplicate = (char *)malloc(length + 1);
+
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= length; i++)
+	{
+		duplicate[i] = str[i];
+	}
+	return (duplicate);
 }
--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,41 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * alloc_grid - Entry point
- * @width: String 1
- * @height: String 2
- * Return: pointer should point to a newly allocated space in memory or NULL
- */
-
 int **alloc_grid(int width, int height)
 {
-	int i;
-	int j;
+	int i, j;
 	int **grid;
-
-	grid = (int **)malloc(height * sizeof(int *));
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
+
+	grid = (int **)malloc(height * sizeof(int *));
+
 	if (grid == NULL)
 	{
 		return (NULL);
 	}
+
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(sizeof(int) * width);
+		grid[i] = (int *)malloc(width * sizeof(int));
 
 		if (grid[i] == NULL)
 		{
-			free(grid[i]);
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
 		}
+
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
 	}
+
 	return (grid);
 }
