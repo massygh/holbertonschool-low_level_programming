9c4df1855431ae0ad0b2b231edb7d7b553440a11
massygh <massyghen1@gmail.com>
2023-11-10 00:59:38 -0800

    test

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * free_grid - Entry point
- *@grid: rows of matrix
- *@height: columns of string
+ * @grid: rows of matrix
+ * @height: columns of string
  * Return: a pointer to a 2 dimensional array of integers or null
  */
+
 void free_grid(int **grid, int height)
 {
 	int i;
