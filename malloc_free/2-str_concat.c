bae4874bef6e2b4ac648d27f66ada1aeeb4ed3e1
massygh <massyghen1@gmail.com>
2023-11-10 01:18:34 -0800

    test

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -22,19 +22,15 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-
 	while (s1[len1] != '\0')
 	{
 		len1++;
 	}
-
 	while (s2[len2] != '\0')
 	{
 		len2++;
 	}
-
 	result = (char *)malloc(len1 + len2 + 1);
-
 	if (result)
 	{
 		for (i = 0; s1[i]; i++)
