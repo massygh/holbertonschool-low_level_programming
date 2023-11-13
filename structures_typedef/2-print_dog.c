9bd75f6ef21759eca14734c78677cd844e177a54
massygh <massyghen1@gmail.com>
2023-11-13 08:19:17 -0800

    test

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdio.h>
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
