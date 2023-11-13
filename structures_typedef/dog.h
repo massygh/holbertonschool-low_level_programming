4850ee9aec2ea92b7c90491819a90f9cbcd4c37e
massygh <massyghen1@gmail.com>
2023-11-13 07:47:48 -0800

    test

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,9 +3,14 @@
 
 typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 } dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
