50bc0a3ecf0b404febab7e618a74ca0f56377813
massygh <massyghen1@gmail.com>
2023-11-13 09:07:01 -0800

    test

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog;
+
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+
+	newDog = malloc(sizeof(dog_t));
+
+	if (newDog == NULL)
+	{
+		return (NULL);
+	}
+
+	newDog->name = strdup(name);
+	newDog->owner = strdup(owner);
+
+	if (newDog->name == NULL || newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog->owner);
+		free(newDog);
+		return (NULL);
+	}
+
+	newDog->age = age;
+
+	return (newDog);
+}
