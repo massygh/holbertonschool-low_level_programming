b8706ba7d86d775c6fd17ad4ece0b19a4fb7a8ab
massygh <massyghen1@gmail.com>
2023-11-14 08:25:21 -0800

    test

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,16 @@
 #include "dog.h"
 #include <stddef.h>
 
+/**
+ * init_dog - prints a name as is
+ * @name: name of the person
+ * @age: age
+ * @d: variable 1
+ * @owner: variable 2
+ *
+ * Return: Nothing.
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
