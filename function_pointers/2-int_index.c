96da9fe8fcf3de068f16fef0841443a5dc379a2e
massygh <massyghen1@gmail.com>
2023-11-14 08:05:41 -0800

    test

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - prints a name as is
+ * @array: name of the person
+ * @size: variable
+ * @cmp: variable 2
+ *
+ * Return: -1.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array != NULL && cmp != NULL && size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
