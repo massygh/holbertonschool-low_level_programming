8ca29f600ec3caf8f54f2795208f84208b589bdb
massygh <massyghen1@gmail.com>
2023-11-14 15:04:15 -0800

    test

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,8 +4,8 @@
 /**
  * print_name - prints a name as is
  * @name: name of the person
+ * @f: variable
  *
- * Return: Nothing.
  */
 
 void print_name(char *name, void (*f)(char *))
--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -7,7 +7,7 @@
  * @size: variable
  * @cmp: variable 2
  *
- * Return: -1.
+ * Return: (-1).
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
