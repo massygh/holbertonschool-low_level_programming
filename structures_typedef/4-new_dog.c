184f1f4715c0357bad4a7b1200b1dd7baa6a280c
massygh <massyghen1@gmail.com>
2023-11-14 16:02:54 -0800

    test

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,9 +1,7 @@
-#include "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * new_dog - prints a name as is
  * @name: name of the person
  * @age : age personne
  * @owner: variable
@@ -11,34 +9,79 @@
  * Return: newDog.
  */
 
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *newDog;
+typedef struct {
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
-	if (name == NULL || owner == NULL)
-	{
-		return (NULL);
+void cleanup_and_exit(dog_t *my_dog) {
+	free(my_dog->name);
+	free(my_dog->owner);
+	free(my_dog);
+	exit(EXIT_FAILURE);
+}
+
+size_t custom_strlen(const char *str) {
+	size_t len = 0;
+	while (str[len] != '\0') {
+		len++;
+	}
+	return len;
+}
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+int main(void) {
+	dog_t *my_dog;
+
+	my_dog = new_dog("Poppy", 3.5, "Bob");
+	if (my_dog == NULL) {
+		printf("Failed to create a new dog\n");
+		exit(EXIT_FAILURE);
 	}
 
-	newDog = malloc(sizeof(dog_t));
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
 
-	if (newDog == NULL)
-	{
-		return (NULL);
+	free(my_dog->name);
+	free(my_dog->owner);
+	free(my_dog);
+
+	return 0;
+}
+
+dog_t *new_dog(char *name, float age, char *owner) {
+	if (name == NULL || owner == NULL) {
+		return NULL;
 	}
 
-	newDog->name = strdup(name);
-	newDog->owner = strdup(owner);
-
-	if (newDog->name == NULL || newDog->owner == NULL)
-	{
-		free(newDog->name);
-		free(newDog->owner);
-		free(newDog);
-		return (NULL);
+	dog_t *new_dog_ptr = malloc(sizeof(dog_t));
+	if (new_dog_ptr == NULL) {
+		return NULL;
 	}
 
-	newDog->age = age;
+	size_t name_len = custom_strlen(name);
+	new_dog_ptr->name = malloc(name_len + 1);
+	if (new_dog_ptr->name == NULL) {
+		cleanup_and_exit(new_dog_ptr);
+	}
 
-	return (newDog);
+	size_t i;
+	for (i = 0; i <= name_len; i++) {
+		new_dog_ptr->name[i] = name[i];
+	}
+
+	new_dog_ptr->age = age;
+
+	size_t owner_len = custom_strlen(owner);
+	new_dog_ptr->owner = malloc(owner_len + 1);
+	if (new_dog_ptr->owner == NULL) {
+		cleanup_and_exit(new_dog_ptr);
+	}
+
+	for (i = 0; i <= owner_len; i++) {
+		new_dog_ptr->owner[i] = owner[i];
+	}
+
+	return new_dog_ptr;
 }
