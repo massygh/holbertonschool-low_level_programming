6426f40a08cb65df701651cd85b4a8525e59c434
massygh <massyghen1@gmail.com>
2023-11-17 02:58:37 -0800

    test

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - print strings
+ * @separator: variable 1
+ * @n: variable
+ * Return: Always sum.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+
+{
+	va_list args;
+	unsigned int i;
+	const char *current_string;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		current_string = va_arg(args, const char *);
+
+		if (current_string == NULL)
+		{
+			printf("(nil)");
+		} else {
+			printf("%s", current_string);
+		}
+
+		if (i < n - 1 && separator != NULL) 
+		{
+			printf("%s", separator);
+		}
+	}
+
+	printf("\n");
+
+	va_end(args);
+}
