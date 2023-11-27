06d97b97bc693005267d761f8a60fcf56b8a3eb6
massygh <massyghen1@gmail.com>
2023-11-27 03:54:08 -0800

    test

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+typedef struct list_s
+{
+	char *str;
+	size_t len;
+	struct list_s *next;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
