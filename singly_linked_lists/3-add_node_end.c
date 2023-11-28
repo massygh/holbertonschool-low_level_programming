21e8ef211317c1179157515ebae900e57c0ff517
massygh <massyghen1@gmail.com>
2023-11-28 03:00:59 -0800

    finish the third task

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,17 +11,18 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
 	list_t *new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 
 	new_node->len = strlen(str);
@@ -34,6 +35,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		list_t *current = *head;
+
 		while (current->next != NULL)
 		{
 			current = current->next;
@@ -41,5 +43,5 @@ list_t *add_node_end(list_t **head, const char *str)
 		current->next = new_node;
 	}
 
-	return new_node;
+	return (new_node);
 }
