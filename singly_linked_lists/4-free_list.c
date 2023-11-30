96e304dbd16efeb251da2af90fbd9ea9577b1f1b
massygh <massyghen1@gmail.com>
2023-11-30 04:09:54 -0800

    test

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,10 +1,11 @@
-#include "main.h"
 #include <stdlib.h>
+#include "lists.h"
 
 /**
  * free_list - frees a list_t list
  * @head: pointer to the list
  */
+
 void free_list(list_t *head)
 {
 	list_t *temp;
