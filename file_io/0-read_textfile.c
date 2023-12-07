5d583f1e6bcec9e469f415b1a54c8166df8b18a2
massygh <massyghen1@gmail.com>
2023-12-07 01:44:23 -0800

    test

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,45 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "main.h"
 /**
- * read_textfile - Reads a specified number of letters
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
- * Return: The actual number of letters read and printed, or 0.
+ * read_textfile - reads a text file
+ * @filename: pointer to the name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: the actual number of letters read and printed, or 0 upon failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-		return (0);
-	int file = open(filename, O_RDONLY);
+	ssize_t op, rd, wr;
+	char *buf;
 
-	if (file == -1)
-		return (0);
-	char *buffer = (char *)malloc(letters);
-
-	if (buffer == NULL)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 	{
-		close(file);
+		free(buf);
 		return (0);
 	}
-	ssize_t bytesRead;
 
-	bytesRead = read(file, buffer, letters);
-	if (bytesRead < 0)
+	op = open(filename, O_RDONLY);
+	rd = read(op, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
+
+	if (op == -1 || rd == -1 || wr != rd)
 	{
-		close(file);
-		free(buffer);
+		free(buf);
 		return (0);
 	}
-	ssize_t bytesWritten;
-
-	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
-	free(buffer);
-	close(file);
-	if (bytesWritten < 0 || (size_t)bytesWritten != (size_t)bytesRead)
-	{
-		return (0);
-	}
-	return (bytesRead);
+	close(op);
+	return (wr);
 }
